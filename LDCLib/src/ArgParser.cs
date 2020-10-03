0d93b1543b182be381ed5175cbea66888d28e636
Birger Skogeng Pedersen <birger.sp@gmail.com>
2020-10-03 22:52:17 +0200

    impl arg parser

---
+++ LDCLib/src/ArgParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDCLib.src
+{
+    public class ArgParser
+    {
+        Dictionary<string, Action<string>> callbacks = new Dictionary<string, Action<string>>();
+
+        public void AddAlternative(string key, Action<string> callback)
+        {
+            callbacks.Add(key, callback);
+        }
+
+        public void AddAlternative(string key, Action callback)
+        {
+            AddAlternative(key, (str) =>
+            {
+                callback();
+            });
+        }
+
+        public void Parse(string argument)
+        {
+            var delimiterIndex = argument.IndexOf("=");
+            string key;
+            string value;
+            if (delimiterIndex == -1)
+            {
+                key = argument;
+                value = "";
+            }
+            else
+            {
+                key = argument.Substring(0, delimiterIndex);
+                value = argument.Substring(delimiterIndex + 1);
+            }
+            callbacks[key](value);
+        }
+    }
+}
---
+++ LDCTest/src/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LDCLib.src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LDCTest.src
+{
+    [TestClass]
+    public class ArgParserTest
+    {
+        [TestMethod]
+        public void Parse()
+        {
+            var data = "";
+            var argParser = new ArgParser();
+            argParser.AddAlternative("foo", (str) =>
+            {
+                data = str;
+            });
+            argParser.Parse("foo=bar");
+            Assert.AreEqual("bar", data);
+        }
+    }
+}
