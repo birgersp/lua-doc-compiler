990847f9d8407a2dcc45757b86ac9a26844b0440
Birger Skogeng Pedersen <birger.sp@gmail.com>
2020-10-04 16:57:51 +0200

    add html generating features

--- LDCApp/src/Program.cs
+++ LDCApp/src/Program.cs
@@ -1,15 +1,115 @@
-﻿using System;
+﻿using LDCLib;
+using LDCLib.src;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LDCApp
 {
     class Program
     {
+        string InDir = ".";
+        string OutDir = "doc";
+        readonly Parser Parser = new Parser();
+
+        static void Log(object obj)
+        {
+            Console.WriteLine(obj);
+        }
+
+        void ParseFile(string fileName)
+        {
+            foreach (var line in File.ReadLines(fileName))
+            {
+                Parser.ParseLine(line);
+            }
+        }
+
+        void ParseArgs(string[] args)
+        {
+            var argParser = new ArgParser();
+            argParser.AddAlternative("inDir", (str) =>
+            {
+                InDir = str;
+            });
+            argParser.AddAlternative("outDir", (str) =>
+            {
+                OutDir = str;
+            });
+            foreach (var arg in args)
+            {
+                argParser.Parse(arg);
+            }
+        }
+
+        void ParseInputFiles()
+        {
+            var files = Directory.GetFiles(InDir);
+            foreach (var file in files)
+            {
+                var fileSuffix = Regex.Replace(file, "^.*\\.", "");
+                if (fileSuffix != "lua")
+                {
+                    continue;
+                }
+                Log($"Reading file: {file}");
+                ParseFile(file);
+            }
+        }
+
+        void Run(string[] args)
+        {
+            ParseArgs(args);
+            ParseInputFiles();
+            WriteToOutFiles();
+            Log("Done");
+        }
+
+        void WriteModule(LuaModule module)
+        {
+            string fileName;
+            if (module.Name == "")
+            {
+                fileName = "root.html";
+            }
+            else
+            {
+                fileName = $"{module.Name}.html";
+            }
+            DocBuilder db = new DocBuilder();
+            db.Begin();
+            db.AddModuleHeader(module.Name);
+            db.AddMajorHeader("Types");
+            var tb = new TableBuilder();
+            tb.Begin();
+            foreach (var type in module.LuaTypes.Values)
+            {
+                //tb.AddRow(type.Name, type.Description);
+                tb.AddRow(type.Name, "");
+            }
+            tb.Finish();
+            db.Add(tb.ToString());
+            db.Finish();
+            File.WriteAllText($"{OutDir}\\{fileName}", db.ToString());
+        }
+
+        void WriteToOutFiles()
+        {
+            Directory.CreateDirectory(OutDir);
+            foreach (var module in Parser.LuaModules.Values)
+            {
+                WriteModule(module);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Run(args);
         }
     }
 }
---
+++ LDCLib/src/html/TableBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDCLib.src
+{
+    public class TableBuilder
+    {
+        readonly StringBuilder SB = new StringBuilder();
+
+        public void Begin()
+        {
+            SB.Append("<table>\n");
+        }
+
+        public void AddRow(params string[] cols)
+        {
+            SB.Append("<tr>\n");
+            foreach (var col in cols)
+            {
+                SB.Append($"<td>{col}</td>\n");
+            }
+            SB.Append("</tr>\n");
+        }
+
+        public void Finish()
+        {
+            SB.Append("</table>\n");
+        }
+
+        public override string ToString()
+        {
+            return SB.ToString();
+        }
+    }
+}
